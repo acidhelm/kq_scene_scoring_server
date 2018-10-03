9c631a18f10d1eccd55d4f454f726656369f1384
Michael Dunn <acidhelm@gmail.com>
2018-10-03 10:31:59 -0700

    Calculate each scene's rank after we calculate the scores This makes us handle ties properly.

--- app/jobs/calculate_scores_job.rb
+++ app/jobs/calculate_scores_job.rb
@@ -15,10 +15,28 @@ class CalculateScoresJob < ApplicationJob
         end
 
         tournament.with_lock do
+            # Create or update `SceneScore` records for each scene.
             scoring_tournament.scene_scores.each do |scene|
                 scene_score = tournament.scene_scores.find_or_initialize_by(name: scene.name)
                 scene_score.score = scene.score
                 scene_score.save
+            end
+
+            # Calculate the rank of each scene.  Teams with the same score get
+            # the same rank.
+            last_rank = -1
+            last_score = -1
+
+            tournament.scene_scores.score_order.each.with_index(1) do |scene, i|
+                if scene.score != last_score
+                    last_score = scene.score
+                    scene.rank = i
+                    last_rank = i
+                else
+                    scene.rank = last_rank
+                end
+
+                scene.save
             end
 
             tournament.save
--- app/models/scene_score.rb
+++ app/models/scene_score.rb
@@ -1,5 +1,7 @@
 class SceneScore < ApplicationRecord
     belongs_to :tournament
 
-    scope :ranked_order, -> { order(score: :desc, name: :asc) }
+    scope :alphabetical_order, -> { order(name: :asc) }
+    scope :score_order, -> { order(score: :desc).alphabetical_order }
+    scope :rank_order, -> { order(rank: :asc).alphabetical_order }
 end
