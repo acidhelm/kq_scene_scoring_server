f57eb54583b464dbce7f1b6323a9f70fcd52f16c
Michael Dunn <acidhelm@gmail.com>
2018-10-03 10:37:26 -0700

    Add a validation for SceneScore#rank

--- app/models/scene_score.rb
+++ app/models/scene_score.rb
@@ -1,6 +1,8 @@
 class SceneScore < ApplicationRecord
     belongs_to :tournament
 
+    validates :rank, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
     scope :alphabetical_order, -> { order(name: :asc) }
     scope :score_order, -> { order(score: :desc).alphabetical_order }
     scope :rank_order, -> { order(rank: :asc).alphabetical_order }
