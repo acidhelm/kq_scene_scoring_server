45af5297a5a96a8697812b07c8eb58705b1be2e5
Michael Dunn <acidhelm@gmail.com>
2018-10-09 13:08:07 -0700

    Add KioskController tests

--- test/controllers/kiosk_controller_test.rb
+++ test/controllers/kiosk_controller_test.rb
@@ -1,8 +1,21 @@
 require "test_helper"
 
 class KioskControllerTest < ActionDispatch::IntegrationTest
+    setup do
+        @tournament = tournaments(:one)
+        @slug = @tournament.slug
+        @bad_slug = @slug.reverse
+    end
+
+    test "View the kiosk for a tournament" do
+        [ @slug, @slug.upcase ].each do |id|
+            get tournament_kiosk_url(id)
+            assert_response :success
+        end
+    end
+
     test "Try to view the kiosk for a non-existant tournament" do
-        get tournament_kiosk_url("foo")
+        get tournament_kiosk_url(@bad_slug)
         assert_response :not_found
     end
 end
