65af861d0aa6b531db74573638a7910cc6b341ec
Michael Dunn <acidhelm@gmail.com>
2018-10-10 14:30:25 -0700

    Add a test to CalculateScoresJobTest that tests the changes made to the tournament object

--- test/jobs/calculate_scores_job_test.rb
+++ test/jobs/calculate_scores_job_test.rb
@@ -1,7 +1,21 @@
-require 'test_helper'
+require "test_helper"
 
 class CalculateScoresJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    test "Test that a tournament is marked as complete" do
+        tournament = tournaments(:live_data_kq25)
+        check_api_key(tournament.user)
+
+        assert_not tournament.complete
+
+        VCR.use_cassette("calc_scores_test_#{tournament.slug}") do
+            CalculateScoresJob.perform_now(tournament)
+        end
+
+        # `TournamentsHelperTest` already tests the code that builds a
+        # `Scoring::Tournament`, so we just have to test the database
+        # calls that `CalculateScoresJob` does after it builds a
+        # `Scoring::Tournament`.
+        # TODO: More checks.
+        assert tournament.complete
+    end
 end
