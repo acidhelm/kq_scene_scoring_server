10f9dd9a3f6f86de0f9f75b0e3ce875bf6c0bc37
Michael Dunn <acidhelm@gmail.com>
2018-10-10 12:14:05 -0700

    Add a test to TournamentsHelperTest that tests with my copy of the KQ 25 brackets

--- test/helpers/tournaments_helper_test.rb
+++ test/helpers/tournaments_helper_test.rb
@@ -1,25 +1,29 @@
 require "test_helper"
 
 class TournamentsHelperTest < ActionView::TestCase
-    setup do
-        @tournament = tournaments(:live_data_4teams)
-    end
-
-    test "Calculate scores for the 4-team tournament" do
+    def verify_scores(tournament)
         scoring_tournament = nil
 
-        VCR.use_cassette("calc_scores_small_tournament") do
+        VCR.use_cassette("calc_scores_test_#{tournament.slug}") do
             scoring_tournament = TournamentsHelper.calc_scores(
-                                     slug: "tvtpeasf", subdomain: nil,
-                                     api_key: @tournament.user.api_key)
+                                     slug: tournament.slug, subdomain: nil,
+                                     api_key: tournament.user.api_key)
         end
 
-        assert_equal scoring_tournament.scene_scores.size, @tournament.scene_scores.size
+        assert_equal scoring_tournament.scene_scores.size, tournament.scene_scores.size
 
         scoring_tournament.scene_scores.each do |s|
-            ranked_scene = @tournament.scene_scores.find_by(name: s.name)
+            ranked_scene = tournament.scene_scores.find_by(name: s.name)
             assert_not_nil ranked_scene
             assert_equal ranked_scene.score, s.score
         end
+    end
+
+    test "Calculate scores for the 4-team tournament" do
+        verify_scores(tournaments(:live_data_4teams))
+    end
+
+    test "Calculate scores for KQ 25" do
+        verify_scores(tournaments(:live_data_kq25))
     end
 end
