505c857dc55dab17406254101b9354c194d72ad9
Michael Dunn <acidhelm@gmail.com>
2018-11-21 22:20:06 -0800

    Generate the meta refresh tag in the kiosk/show view, not the controller

--- app/controllers/kiosk_controller.rb
+++ app/controllers/kiosk_controller.rb
@@ -6,9 +6,6 @@ class KioskController < ApplicationController
     def show
         # The user can set the refresh time by passing the time in seconds as
         # the `t` parameter.
-        refresh_time = params[:t] || Rails.configuration.kiosk_refresh_time
-
-        @refresh_tag = @tournament.complete ? "" :
-                         "<meta http-equiv='refresh' content='#{refresh_time}'>".html_safe
+        @refresh_time = params[:t] || Rails.configuration.kiosk_refresh_time
     end
 end
