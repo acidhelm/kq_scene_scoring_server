165cfd4e224140b112de953a9447ef92801f7a12
Michael Dunn <acidhelm@gmail.com>
2019-01-31 16:25:59 -0800

    Write tests for the tournaments/show view

--- test/system/tournaments_test.rb
+++ test/system/tournaments_test.rb
@@ -63,4 +63,48 @@ class TournamentsTest < ApplicationSystemTestCase
             end
         end
     end
+
+    EXPECTED_SCORES = [ %w(New\ York 391.5), %w(Chicago 370.5), %w(Charlotte 316.5),
+                        %w(Minneapolis 292.0), %w(Portland 275.0), %w(Columbus 219.5),
+                        %w(Kansas\ City 160.5), %w(Seattle 88.0), %w(Madison 73.0),
+                        %w(Austin 46.0), %w(San\ Francisco 37.0), %w(CHA 21.0),
+                        %w(Los\ Angeles 15.5), %w(Iowa 11.0) ]
+
+    test "Check the show-tournament page" do
+        tournament = tournaments(:live_data_kq25)
+
+        visit user_tournament_url(@user, tournament)
+
+        assert_selector "h1", exact_text: tournament.title
+        assert_selector "p strong", exact_text: "Challonge ID:"
+        assert_selector "p", text: tournament.slug
+        assert_selector "p strong", exact_text: "Subdomain:"
+        assert_selector "p", text: tournament.subdomain if tournament.subdomain.present?
+        assert_selector "p strong", exact_text: "Complete?"
+        assert_selector "p", text: tournament.complete ? "Yes" : "No"
+        assert_button "Calculate scores now", exact: true
+
+        assert_selector "th", exact_text: "Rank"
+        assert_selector "th", exact_text: "Scene"
+        assert_selector "th", exact_text: "Score"
+
+        page.all("tbody tr").each_with_index do |tr, row|
+            tr.all("td").each_with_index do |td, i|
+                case i
+                    when 0
+                        assert_equal (row + 1).to_s, td.text
+                    when 1
+                        assert_equal EXPECTED_SCORES[row][0], td.text
+                    when 2
+                        assert_equal EXPECTED_SCORES[row][1], td.text
+                end
+            end
+        end
+
+        assert_link "Change this tournament's settings",
+                    href: edit_user_tournament_path(@user, tournament), exact: true
+        assert_link "Back to the tournament list",
+                    href: user_tournaments_path(@user), exact: true
+        assert_link "Log out", href: logout_path, exact: true
+    end
 end
