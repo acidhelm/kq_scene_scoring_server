7ca5d1ae93a3d2bd9e01c356fbebf205b9831c9a
Michael Dunn <acidhelm@gmail.com>
2019-02-07 14:21:27 -0800

    Add more tests to the CreateTournamentTest system test Check for more UI elements.

--- test/system/create_tournament_test.rb
+++ test/system/create_tournament_test.rb
@@ -8,6 +8,16 @@ class CreateTournamentTest < ApplicationSystemTestCase
 
         click_on "Calculate scores for a new tournament"
 
+        assert_selector "label", exact_text: "Title:"
+        assert_field id: "tournament_title", type: "text"
+        assert_selector "label", text: "Challonge ID:"
+        assert_field id: "tournament_slug", type: "text"
+        assert_selector "label", text: "Subdomain:"
+        assert_field id: "tournament_subdomain", type: "text"
+        assert_button "Create Tournament", exact: true
+        assert_link "Back to the tournament list", exact: true,
+                    href: user_tournaments_path(@user)
+
         fill_in :tournament_title, with: "Test BB3 tourney"
         fill_in :tournament_slug, with: "bb3wc"
 
